ffec4e1849fdb1e49a89e8cdf222a8a35c43b329
Joachim Bengtsson <joachimb@gmail.com>
2011-10-20 20:18:31 +0200

    Initial Commit

---
+++ touchid/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  touchid
+//
+//  Created by Joachim Bengtsson on 2011-10-20.
+//  Copyright (c) 2011 Third Cog Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
